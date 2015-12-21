caac835ecb690424388c89e8c714fff981d76bbe
Nyctef <nyctef@nyctef.com>
2015-12-21 18:07:08 +0000

    another test class

--- ps_helpers.ps1
+++ ps_helpers.ps1
@@ -1,9 +1,15 @@
 # Create class + namespace with folder if necessary
 function makeclass($name) {
+    $dirName = '.'
+    $className = $name
+
     $splitName = $name.split(@('.', '/', '\'))
-    $dirName = "$([IO.Path]::Combine($splitName[0..($splitName.Length-2)]))"
-    $className = "$($splitName[-1])"
-    mkdir -force $dirName
+    if ($splitName.length -gt 1) {
+        $dirName = "$([IO.Path]::Combine($splitName[0..($splitName.Length-2)]))"
+        $className = "$($splitName[-1])"
+        mkdir -force $dirName
+    }
+
     push-location $dirName
     yo aspnet:Class $className
     pop-location
---
+++ tests/HaulerTrawler.Tests/TypeIdGeneratorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaulerTrawler.Tests
+{
+    public class TypeIdGeneratorTests
+    {
+        public TypeIdGeneratorTests()
+        {
+        }
+    }
+}
