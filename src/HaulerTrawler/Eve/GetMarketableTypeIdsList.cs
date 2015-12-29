ec72274ddb3fbc97639f543893ef4dbefc40ca66
Nyctef <nyctef@nyctef.com>
2015-12-29 15:54:21 +0000

    first pass at GetMarketableTypeIdsList

--- src/HaulerTrawler/Eve/GetMarketableTypeIdsList.cs
+++ src/HaulerTrawler/Eve/GetMarketableTypeIdsList.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using HaulerTrawler.Interfaces;
+using Newtonsoft.Json.Linq;
 
 namespace HaulerTrawler.Eve
 {
@@ -10,7 +12,26 @@ namespace HaulerTrawler.Eve
     {
         public IEnumerable<TypeId> Get()
         {
-            return null;
+            var crestBaseUrl = @"https://public-crest.eveonline.com/";
+            dynamic crestBase = GetJson(crestBaseUrl);
+            var marketTypesUrl = (string)crestBase.marketTypes.href;
+            dynamic marketTypes = GetJson(marketTypesUrl);
+            var result = new List<TypeId>();
+            foreach (var item in marketTypes.items)
+            {
+                result.Add(new TypeId((int)item.type.id, (string)item.type.name));
+            }
+            return result;
+        }
+
+        private static dynamic GetJson(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                // TODO async
+                var json = client.GetStringAsync(url).Result;
+                return JObject.Parse(json);
+            }
         }
     }
 }
