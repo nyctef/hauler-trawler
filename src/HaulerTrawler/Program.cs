f71ca90dd712e74fe4d5f3af7cd47a2071bb1d38
Nyctef <nyctef@nyctef.com>
2015-12-30 11:53:08 +0000

    start building dependency graph in Program.cs

--- src/HaulerTrawler/Program.cs
+++ src/HaulerTrawler/Program.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HaulerTrawler.Interfaces;
+using HaulerTrawler.Eve;
+using HaulerTrawler.Utils;
 
 namespace HaulerTrawler
 {
@@ -9,6 +12,25 @@ namespace HaulerTrawler
     {
         public static void Main(string[] args)
         {
+            var prog = new HaulerTrawler(
+                    new TradeGenerator(
+                        new TypeIdGenerator(
+                            new GetMarketableTypeIdsList(),
+                            new RandomChooser()
+                            ),
+                        new PriceChecker(),
+                        new SolarSystemFactory(
+                            new GetSolarSystemIds()
+                            ),
+                        new SolarSystemRouteFinder()
+                        ),
+                    null, // new TradeAnalyzer(),
+                    null // new Notifier()
+                    );
+
+            prog.DoTheThing();
+
+
             Console.WriteLine("Hello World");
             Console.Read();
         }
